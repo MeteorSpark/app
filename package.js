5c41033c0120cffd1d674061d79cd0c4e009d10e
Daniel C <daniel@meteorspark.com>
2024-04-23 10:48:30 +0700

    Bump version to v0.4.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorspark:app',
   summary: 'Exposes to the global namespace\'s `APP`, an event emitter based object for project organization',
-  version: '0.3.0',
+  version: '0.4.0',
   git: 'https://github.com/MeteorSpark/app'
 });
 
@@ -10,14 +10,14 @@ server = 'server';
 client = 'client';
 
 Package.onUse(function (api) {
-  api.versionsFrom('0.9.4');
+  api.versionsFrom('2.15');
 
   api.use('underscore', both);
 
   api.use('meteorspark:util@0.2.0', both);
-  api.use('raix:eventemitter@0.1.1', both);
+  api.use('raix:eventemitter@1.0.0', both);
 
-  api.add_files('app.js', both);
+  api.addFiles('app.js', both);
 
   api.export('APP', both);
 });
